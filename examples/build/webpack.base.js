c578341ffb354f9263637a5f1ce24b36e47c3018
wbccb <wbccb18814122@163.com>
2024-08-01 01:33:01 +0800

    examples: 修复babel-loader配置错误

--- examples/build/webpack.base.js
+++ examples/build/webpack.base.js
@@ -1,47 +1,43 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.tsx'),
-    output: {
-        path: path.resolve(__dirname, '../dist'), // 打包后的代码放在dist目录下
-        filename: '[name].[hash:8].js', // 打包的文件名
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
-            inject: true, // 自动注入静态资源
-        })
-    ],
-    resolve: {
-        // 配置 extensions 来告诉 webpack 在没有书写后缀时，以什么样的顺序去寻找文件
-        extensions: ['.mjs', '.js', '.json', '.jsx', '.ts', '.tsx'], // 如果项目中只有 tsx 或 ts 可以将其写在最前面
-    },
-    module: {
-        rules: [
-            {
-                test: /.(jsx?)|(tsx?)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: 'iOS 9, Android 4.4, last 2 versions, > 0.2%, not dead', // 根据项目去配置
-                                    useBuiltIns: 'usage', // 会根据配置的目标环境找出需要的polyfill进行部分引入
-                                    corejs: 3, // 使用 core-js@3 版本
-                                },
-                            ],
-                            ['@babel/preset-typescript'],
-                            ['@babel/preset-react', {
-                                "runtime": "automatic"
-                            }],
-                        ],
-                    },
-                },
-            },
-        ],
-    },
-}
+	entry: path.resolve(__dirname, "../src/index.tsx"),
+	output: {
+		path: path.resolve(__dirname, "../dist"), // 打包后的代码放在dist目录下
+		filename: "[name].[hash:8].js", // 打包的文件名
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "../public/index.html"), // 模板取定义root节点的模板
+			inject: true, // 自动注入静态资源
+		}),
+	],
+	resolve: {
+		// 配置 extensions 来告诉 webpack 在没有书写后缀时，以什么样的顺序去寻找文件
+		extensions: [".mjs", ".js", ".json", ".jsx", ".ts", ".tsx"], // 如果项目中只有 tsx 或 ts 可以将其写在最前面
+	},
+	module: {
+		rules: [
+			{
+				test: /.(jsx?)|(tsx?)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							["@babel/preset-env"],
+							["@babel/preset-typescript"],
+							[
+								"@babel/preset-react",
+								{
+									"runtime": "automatic",
+								},
+							],
+						],
+					},
+				},
+			},
+		],
+	},
+};
