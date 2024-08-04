b586fe0e27f0291c010132385cfe1e1d031aaaa8
wbccb <wbccb18814122@163.com>
2024-08-04 16:56:41 +0800

    packages: 最小堆构建逻辑完善

--- packages/shared/src/MinHeap.ts
+++ packages/shared/src/MinHeap.ts
@@ -39,25 +39,77 @@ class MinHeap {
 		if (this.size <= 0) {
 			return undefined;
 		}
-		const res = this.array[1];
-		// TODO 执行shiftDown()操作
-		return res;
+		if (this.size === 1) {
+			this.size = 0;
+			return this.array[1];
+		}
+		this.array[1] = this.array[this.size];
+		this.size--;
+		this.shiftDown(1);
 	}
 
 	peek(): Node | undefined {
-		if (this.size <= 0) {
-			return undefined;
+		if (this.size > 0) {
+			return this.array[1];
 		}
-		return this.array[this.size - 1];
 	}
 
-	shiftUp(i: number) {}
-	shiftDown(i: number) {}
+	shiftUp(index: number) {
+		while (this.getParentIndex(index) >= 1) {
+			const parentIndex = this.getParentIndex(index);
+			let currentValue = this.array[index];
+			let parentValue = this.array[parentIndex];
+			if (currentValue < parentValue) {
+				this.swap(this.array, index, parentIndex);
+				index = parentIndex;
+			} else {
+				break;
+			}
+		}
+	}
+	shiftDown(index: number) {
+		let current = index;
+		while (current <= this.size) {
+			const leftIndex = this.getChildLeft(current);
+			const rightIndex = this.getChildRight(current);
+			let tempIndex = current;
+			if (
+				leftIndex <= this.size &&
+				this.array[leftIndex] < this.array[tempIndex]
+			) {
+				tempIndex = leftIndex;
+			}
+			if (
+				rightIndex <= this.size &&
+				this.array[rightIndex] < this.array[tempIndex]
+			) {
+				tempIndex = rightIndex;
+			}
+			if (tempIndex === current) {
+				break;
+			}
+			this.swap(this.array, current, tempIndex);
+			current = tempIndex;
+		}
+	}
 
-	swap(i: number, j: number) {
-		const temp = this.array[i];
-		this.array[i] = this.array[j];
-		this.array[j] = temp;
+	swap(nums: Array<Task>, i: number, j: number) {
+		const temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+
+	getParentIndex(index: number) {
+		// root=1
+		// parent=2 => 4 5
+		// parent=3 => 6 7
+		return Math.floor(index / 2);
+	}
+	getChildLeft(index: number) {
+		return index * 2;
+	}
+	getChildRight(index: number) {
+		return index * 2 + 1;
 	}
 }
 
