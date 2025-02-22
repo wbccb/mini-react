d6c464e31eac45e8709c7b5c303563469b952787
wbccb <wbccb18814122@163.com>
2025-02-22 10:56:07 +0800

    packages(合成事件): 原生事件触发=>处理事件队列queue

--- packages/react-dom/src/events/DOMPluginEventSystem.ts
+++ packages/react-dom/src/events/DOMPluginEventSystem.ts
@@ -8,6 +8,7 @@ import { EventSystemFlags, IS_CAPTURE_PHASE } from "./EventSystemFlags";
 import { createEventListenerWrapperWithPriority } from "./ReactDOMEventListener";
 import { addEventBubbleListener, addEventCaptureListener } from "./EventListener";
 import { AnyNativeEvent } from "./PluginModuleType";
+import getEventTarget from "./getEventTarget";
 
 type DispatchListener = {
 	instance: null | Fiber;
@@ -136,6 +137,113 @@ function dispatchEventForPluginEventSystem(
 	nativeEvent: AnyNativeEvent,
 	targetInst: null | Fiber,
 	targetContainer: EventTarget,
-): void {}
+): void {
+	const ancestorInst = targetInst;
+	// 省略检测当前 fiber (不断fiber=fiber.return)找到的HostRoot对应的DOM跟目前传入的targetContainerNode是否不同，不同则需要重新寻找
+
+	// 开始正式派发事件
+	dispatchEventsForPlugins(
+		domEventName,
+		eventSystemFlags,
+		nativeEvent,
+		ancestorInst,
+		targetContainer,
+	);
+}
+
+function dispatchEventsForPlugins(
+	domEventName: DOMEventName,
+	eventSystemFlags: EventSystemFlags,
+	nativeEvent: AnyNativeEvent,
+	targetInst: null | Fiber,
+	targetContainer: EventTarget,
+): void {
+	const nativeEventTarget = getEventTarget(nativeEvent);
+	const dispatchQueue: DispatchQueue = [];
+	extractEvents(
+		dispatchQueue,
+		domEventName,
+		targetInst,
+		nativeEvent,
+		nativeEventTarget,
+		eventSystemFlags,
+		targetContainer,
+	);
+	processDispatchQueue(dispatchQueue, eventSystemFlags);
+}
+
+function extractEvents(
+	dispatchQueue: DispatchQueue,
+	domEventName: DOMEventName,
+	targetInst: null | Fiber,
+	nativeEvent: AnyNativeEvent,
+	nativeEventTarget: null | EventTarget,
+	eventSystemFlags: EventSystemFlags,
+	targetContainer: EventTarget,
+) {
+	SimpleEventPlugin.extractEvents(
+		dispatchQueue,
+		domEventName,
+		targetInst,
+		nativeEvent,
+		nativeEventTarget,
+		eventSystemFlags,
+		targetContainer,
+	);
+
+	//...省略好多 XXXPlugin.extractEvent()
+}
+
+export function processDispatchQueue(
+	dispatchQueue: DispatchQueue,
+	eventSystemFlags: EventSystemFlags,
+): void {
+	const inCapturePhase = (eventSystemFlags & IS_CAPTURE_PHASE) !== 0;
+	for (let i = 0; i < dispatchQueue.length; i++) {
+		const {event, listeners} = dispatchQueue[i];
+		processDispatchQueueItemsInOrder(event, listeners, inCapturePhase);
+	}
+}
+
+
+function processDispatchQueueItemsInOrder(
+	event: ReactSyntheticEvent,
+	dispatchListeners: Array<DispatchListener>,
+	inCapturePhase: boolean,
+): void {
+	let previousInstance;
+	if (inCapturePhase) {
+		for (let i = dispatchListeners.length - 1; i >= 0; i--) {
+			const {instance, currentTarget, listener} = dispatchListeners[i];
+			if (instance !== previousInstance && event.isPropagationStopped()) {
+				return;
+			}
+			executeDispatch(event, listener, currentTarget);
+			previousInstance = instance;
+		}
+	} else {
+		for (let i = 0; i < dispatchListeners.length; i++) {
+			const {instance, currentTarget, listener} = dispatchListeners[i];
+			if (instance !== previousInstance && event.isPropagationStopped()) {
+				return;
+			}
+			executeDispatch(event, listener, currentTarget);
+			previousInstance = instance;
+		}
+	}
+}
+
+function executeDispatch(
+	event: ReactSyntheticEvent,
+	listener: Function,
+	currentTarget: EventTarget,
+): void {
+	const type = event.type || 'unknown-event';
+	event.currentTarget = currentTarget;
+	// @ts-ignore
+	listener.call(this, type, undefined, event);
+	event.currentTarget = null;
+}
+
 
 export { listenToAllSupportedEvents, dispatchEventForPluginEventSystem };
--- packages/react-dom/src/events/plugins/SimpleEventPlugin.ts
+++ packages/react-dom/src/events/plugins/SimpleEventPlugin.ts
@@ -1,8 +1,12 @@
-import { registerSimpleEvents } from "../DOMEventProperties";
 import { DOMEventName } from "../DOMEventNames";
 import { Fiber } from "react-reconciler";
 import type { DispatchQueue } from "../DOMPluginEventSystem";
 import type { AnyNativeEvent, EventSystemFlags } from "../../events/PluginModuleType";
+import {
+	topLevelEventsToReactNames,
+	registerSimpleEvents,
+} from '../DOMEventProperties';
+
 
 function extractEvents(
 	dispatchQueue: DispatchQueue,
@@ -12,6 +16,13 @@ function extractEvents(
 	nativeEventTarget: null | EventTarget,
 	eventSystemFlags: EventSystemFlags,
 	targetContainer: EventTarget,
-) {}
+) {
+
+	// 先从 topLevelEventsToReactNames 中利用原生的DOM事件名称获取React事件名称
+	// 触发accumulateSinglePhaseListeners()传入 fiber + React事件名称（比如onClick），通过fiber.props上的属性，获取对应的监听方法
+	// 创建合成事件对象_event = new SyntheticEventCtor()
+	// 将合成事件对象 + fiber.props 获取到的回调函数数组加入到 dispatchQueue 中
+
+}
 
 export { registerSimpleEvents as registerEvents, extractEvents };
